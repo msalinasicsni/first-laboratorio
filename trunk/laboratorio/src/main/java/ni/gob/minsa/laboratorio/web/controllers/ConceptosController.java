6e91999fd71005fb0c93faadfab6316a75dead7c
msalinas@icsnicaragua.org <msalinas@icsnicaragua.org@0e4f6247-f613-ec99-d834-6327aa50b51c>
2015-02-03 18:13:13 +0000

    - Inicio administración de conceptos para ingreso de resultados

---
+++ trunk/laboratorio/src/main/java/ni/gob/minsa/laboratorio/web/controllers/ConceptosController.java
@@ -0,0 +1,15 @@
+package ni.gob.minsa.laboratorio.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by FIRSTICT on 2/3/2015.
+ */
+
+@Controller
+@RequestMapping("administracion/conceptos")
+public class ConceptosController {
+
+
+}
