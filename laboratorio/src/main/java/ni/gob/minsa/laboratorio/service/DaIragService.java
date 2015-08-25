a666a8685ca2eb88b2b94085ff725b446f43eaa1
souynemperatriz <souynemperatriz@gmail.com>
2015-08-25 10:07:46 -0600

    Ficha Irag PDF

---
+++ laboratorio/src/main/java/ni/gob/minsa/laboratorio/service/DaIragService.java
@@ -0,0 +1,40 @@
+package ni.gob.minsa.laboratorio.service;
+
+import ni.gob.minsa.laboratorio.domain.irag.DaIrag;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by souyen-ics
+ */
+@Service("daIragService")
+@Transactional
+public class DaIragService {
+
+    static final Logger logger = LoggerFactory.getLogger(DaIragService.class);
+
+    @Resource(name="sessionFactory")
+    private SessionFactory sessionFactory;
+
+
+    /**
+     * Retorna Ficha de Vigilancia Integrada
+     * @param id
+     */
+    public DaIrag getFormById(String id) {
+
+        Session session = sessionFactory.getCurrentSession();
+
+        Query query = session.createQuery("FROM DaIrag vi where vi.idNotificacion = '" + id + "'");
+        return (DaIrag) query.uniqueResult();
+
+    }
+
+}
